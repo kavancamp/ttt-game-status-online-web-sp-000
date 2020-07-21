e553baa514bd5097af80bee7a38aefdb9ce0af21
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-21 12:37:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,9 +20,9 @@ def won?(board)
    win_index_1 = win_combination[1]
    win_index_2 = win_combination[2]
 
-      position_1 = board[index_0]
-      position_2 = board[index_1]
-      position_3 = board[index_2]
+      position_1 = board[win_index_0]
+      position_2 = board[win_index_1]
+      position_3 = board[win_index_2]
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination
