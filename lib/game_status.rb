e9d56d874b7a060c80b5d595955a90b148d651d6
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-21 12:19:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+def WIN_COMBINATIONS[
+  [0,1,2], # top
+  [3,4,5], # middle 
+  [6,7,8], # bottom
+  [0,3,6], # left
+  [1,4,7], # center 
+  [2,5,8], # right
+  [0,4,8], # left diagonal 
+  [6,4,2] ] # right diagonal
+end
